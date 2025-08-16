96117435fcae33d30480f82c87132ef73a56f0e6
Grydr <jalucahyoseno@gmail.com>
2025-08-16 17:37:52 +0700

    extra credit done

--- script.js
+++ script.js
@@ -2,25 +2,54 @@ const gridContainer = document.querySelector("#grid-container");
 const resetButton = document.querySelector(".reset-button");
 const resizeButton = document.querySelector(".resize-button");
 
+function randomizeColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+let mouseDown = false;
+document.body.onmousedown = () => { mouseDown = true };
+document.body.onmouseup = () => { mouseDown = false };
+
+function changeColor(event, box) {
+    if (event.type === "mouseover" && !mouseDown) return;
+    box.style.backgroundColor = randomizeColor();
+
+    const curOpacity = parseFloat(box.style.opacity);
+    if (curOpacity < 1) {
+        box.style.opacity = `${curOpacity + 0.1}`;
+    }
+}
+
+function calculateGridSize() {
+    const gridBorderWidth = parseInt(getComputedStyle(gridContainer).borderWidth) * 2;
+    return gridContainer.getBoundingClientRect().width - gridBorderWidth;
+}
+
 function makeGrid(gridSize) {
     gridContainer.innerHTML = "";
-    const boxSize = 800 / gridSize;
+    console.log(getComputedStyle(gridContainer))
+    const boxSize = calculateGridSize() / gridSize;
 
     for (let i = 0; i < gridSize * gridSize; i++) {
         const box = document.createElement("div");
         box.classList.add("box");
         box.style.width = `${boxSize}px`;
         box.style.height = `${boxSize}px`;
+        box.style.opacity = "0.1";
+
+        box.addEventListener("mouseover", e => {
+            e.preventDefault();
+            changeColor(e, box);
+        });
+        box.addEventListener("mousedown", e => {
+            e.preventDefault();
+            changeColor(e, box);
+        });
         gridContainer.append(box);
     }
-
-    const allBoxes = gridContainer.querySelectorAll(".box");
-    allBoxes.forEach(box => {
-        box.addEventListener("mouseover", e => {
-            // console.log("hovered over " + box.getAttribute("class"));
-            box.style.backgroundColor = "black";
-        });
-    });
 }
 
 makeGrid(16);
